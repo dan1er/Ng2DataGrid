cbcef58d3bd8b6d6bb82991e10b80d2f1459bff9
Danier Estevez <danier.estevez@uruit.com>
2017-02-25 21:58:33 -0300

    Added drag and drop behaviour (#1) (#2)
    * Added drag and drop behaviour
    
    * Removed unused import

--- src/app/data-grid/data-grid-row/data-grid-row.component.ts
+++ src/app/data-grid/data-grid-row/data-grid-row.component.ts
@@ -1,14 +1,16 @@
 import {
-    Component, Input, Output, EventEmitter, TemplateRef, OnInit
+    Component, Input, Output, EventEmitter, TemplateRef, OnInit, ElementRef, NgZone, OnChanges, SimpleChanges
 } from '@angular/core';
-import { Column } from "../data-grid-column/data-grid-column.component";
-import {RowMarkData, RowData} from "../data-grid/data-grid.component";
+import {Column} from "../data-grid-column/data-grid-column.component";
+import {RowMarkData, RowData, RowDragEndedEvent} from "../data-grid/data-grid.component";
+import {fromEvent} from 'rxjs/observable/fromEvent';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'data-grid-row',
     template: `
-      <div class="data-table-body-row">
-          <div class="data-table-body-row-columns" (click)="onColumnsRowClicked()">
+      <div class="data-table-body-row" [draggable]="allowRowsReorder">
+          <div class="data-table-body-row-columns" [ngStyle]="rowData.relocated && relocatedStyles" (click)="onColumnsRowClicked()">
               <template [ngIf]="rowMarkField && rowData.data[rowMarkField]">
                     <div class="column-marker" [style.border-color]="rowData.data[rowMarkField].color">
                     <span *ngIf="rowData.data[rowMarkField].letter">{{rowData.data[rowMarkField].letter}}</span>      
@@ -40,20 +42,53 @@ import {RowMarkData, RowData} from "../data-grid/data-grid.component";
           </div>
       </div>
   `,
-    styleUrls: ['./data-grid-row.component.less'],
+    styleUrls: ['./data-grid-row.component.less']
 })
-export class DataGridRowComponent implements OnInit {
-    @Input() public columns: Column[];
+export class DataGridRowComponent implements OnInit, OnChanges {
+    @Input() public readonly columns: Column[];
     @Input() public rowData: RowData;
-    @Input() public showSelectionInput: boolean;
-    @Input() public selectionMode: string;
-    @Input() public expandTemplate: TemplateRef<any>;
-    @Input() public rowMarkField: string;
+    @Input() public readonly rowIndex: number;
+    @Input() public readonly showSelectionInput: boolean;
+    @Input() public readonly selectionMode: string;
+    @Input() public readonly expandTemplate: TemplateRef<any>;
+    @Input() public readonly rowMarkField: string;
+    @Input() public readonly allowRowsReorder: string;
+    @Input() public readonly relocatedStyles: string[];
     @Output() public onRowSelectionChanged: EventEmitter<any> = new EventEmitter();
     @Output() public onRowExpanded: EventEmitter<any> = new EventEmitter();
+    @Output() public onRowDragEnded: EventEmitter<RowDragEndedEvent> = new EventEmitter();
+
+    private element: HTMLDivElement;
+    private draggingSubscribersInitialized: boolean;
+    private onDragOver$: Subscription;
+    private onDragLeave$: Subscription;
+    private onDragStart$: Subscription;
+    private onDrop$: Subscription;
+
+    constructor(private el: ElementRef, private zone: NgZone) {
+        this.element = this.el.nativeElement;
+    }
 
     public ngOnInit(): void {
         this.initRowMarkData();
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes["allowRowsReorder"]) {
+            if (this.allowRowsReorder) {
+                if (!this.draggingSubscribersInitialized) {
+                    this.subscribeToDragEvents();
+
+                    this.draggingSubscribersInitialized = true;
+                }
+            } else {
+                if (this.draggingSubscribersInitialized) {
+                    this.unSubscribeFromDragEvents();
+
+                    this.draggingSubscribersInitialized = false;
+                }
+            }
+        }
     }
 
     public onRowSelectedChanged($event: boolean): void {
@@ -98,5 +133,64 @@ export class DataGridRowComponent implements OnInit {
                 }
             }
         }
+    }
+
+    private subscribeToDragEvents(): void {
+        this.zone.runOutsideAngular(() => {
+            this.onDragStart$ = this.onDragStart();
+            this.onDragOver$ = this.onDragOver();
+            this.onDragLeave$ = this.onDragLeave();
+            this.onDrop$ = this.onDrop();
+        });
+    }
+
+    private unSubscribeFromDragEvents(): void {
+        this.onDragStart$.unsubscribe();
+        this.onDragOver$.unsubscribe();
+        this.onDragLeave$.unsubscribe();
+        this.onDrop$.unsubscribe();
+    }
+
+    private onDragStart(): Subscription {
+        return fromEvent(this.element, "dragstart")
+            .subscribe(($event: DragEvent) => {
+                $event.stopPropagation();
+
+                $event.dataTransfer.setData("index", `${this.rowIndex}`);
+                $event.dataTransfer.effectAllowed = "move";
+            });
+    }
+
+    private onDragOver(): Subscription {
+        return fromEvent(this.element, "dragover")
+            .subscribe(($event: DragEvent) => {
+                $event.preventDefault();
+
+                $event.dataTransfer.dropEffect = "move";
+
+                if (!this.element.classList.contains("row-dragging-over")) {
+                    this.element.classList.add("row-dragging-over");
+                }
+            });
+
+    }
+
+    private onDragLeave(): Subscription {
+        return fromEvent(this.element, "dragleave")
+            .subscribe(() => {
+                this.element.classList.remove("row-dragging-over");
+            });
+    }
+
+    private onDrop(): Subscription {
+        return fromEvent(this.element, "drop")
+            .subscribe(($event: DragEvent) => {
+                $event.preventDefault();
+
+                let index = $event.dataTransfer.getData("index");
+                this.onRowDragEnded.emit({ currentIndex: +index, nextIndex: this.rowIndex });
+
+                this.element.classList.remove("row-dragging-over");
+            });
     }
 }
